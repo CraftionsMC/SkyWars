ddec5b83dd0b70a8dad7c5ecc5d9c12c092f9ca6
MCTzOCK <dev@mctzock.de>
2021-06-22 13:36:41 +0200

    Added basic files

---
+++ src/main/java/net/craftions/skywars/SkyWars.kt
@@ -0,0 +1,13 @@
+package net.craftions.skywars
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SkyWars : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
